bec8062bd473bc338e5fee7fd6b7b9a293c46cce
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-09-08 22:26:02 +0300

    Final polish

--- blog/views.py
+++ blog/views.py
@@ -23,10 +23,12 @@ def serialize_post(post):
 
 def index(request):
     """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
+    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM
+    нужно их оптимизировать как раз на примере этого сайта.
     """
     all_posts = Post.objects.prefetch_related('author')
-    popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:3]
+    popular_posts = all_posts.annotate(
+        likes_count=Count('likes')).order_by('-likes_count')[:3]
     fresh_posts = all_posts.order_by('-published_at')[:5]
 
     context = {
@@ -38,7 +40,8 @@ def index(request):
 
 def post_detail(request, slug):
     """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
+    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM
+    нужно их оптимизировать как раз на примере этого сайта.
     """
     post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post)
@@ -69,7 +72,8 @@ def post_detail(request, slug):
 
 def contact(request):
     """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
+    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM
+    нужно их оптимизировать как раз на примере этого сайта.
     """
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
